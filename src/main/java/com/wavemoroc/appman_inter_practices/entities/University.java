a163fc19661a8c6738c8490a6fb13b68cfa0fe59
wavemoroc <patipan.wavemoroc@mail.kmutt.ac.th>
2021-11-02 19:07:42 +0700

    [Implement] implement University entity

--- src/main/java/com/wavemoroc/appman_inter_practices/entities/University.java
+++ src/main/java/com/wavemoroc/appman_inter_practices/entities/University.java
@@ -1,11 +1,33 @@
 package com.wavemoroc.appman_inter_practices.entities;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
-
+@Getter
+@Setter
+@Table(name = "universities")
 public class University {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long uniId;
+    private String uniName;
+    private String uniAddress;
+
+    @ManyToMany
+    @JoinTable(name = "studentAdmission",
+            joinColumns = {@JoinColumn(name = "uniId", referencedColumnName = "uniId")},
+            inverseJoinColumns = {@JoinColumn(name = "stuId", referencedColumnName = "stuId")}
+    )
+    private List<Student> studentList;
+
+    public University(String uniName, String uniAddress) {
+        this.uniName = uniName;
+        this.uniAddress = uniAddress;
+    }
 }
---
+++ src/main/java/com/wavemoroc/appman_inter_practices/repositories/UniversityRepository.java
@@ -0,0 +1,6 @@
+package com.wavemoroc.appman_inter_practices.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UniversityRepository extends JpaRepository<UniversityRepository,Long> {
+}
