e35ddfab253ff19170d0ca0a49ff69dbc790cc1e
wavemoroc <patipan.wavemoroc@mail.kmutt.ac.th>
2021-11-03 00:28:37 +0700

    [Fix] fix not handle exception presentation

--- src/main/java/com/wavemoroc/appmanInterPractices/exceptions/ExceptionRes.java
+++ src/main/java/com/wavemoroc/appmanInterPractices/exceptions/ExceptionRes.java
@@ -1,8 +1,10 @@
 package com.wavemoroc.appmanInterPractices.exceptions;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 
+@Getter
 public class ExceptionRes {
     private Integer statusCode;
     private HttpStatus httpStatus;
